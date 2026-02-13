6fab7764bc4a8e3ac66991bb7bf59c7d22e765f6
Suyoung-Kim <150898767+suweeety@users.noreply.github.com>
2026-02-13 21:42:22 +0900

    [Unrated] Title: [기본] 6일차 - 피자 굽기 (제출용), Time: 80  ms, Memory: 55936  KB - DashHub

--- SWEA/Unrated/10724. ［기본］ 6일차 － 피자 굽기 （제출용）/［기본］ 6일차 － 피자 굽기 （제출용）.py
+++ SWEA/Unrated/10724. ［기본］ 6일차 － 피자 굽기 （제출용）/［기본］ 6일차 － 피자 굽기 （제출용）.py
@@ -1,18 +1,24 @@
+from collections import deque
+
 for tc in range(1, int(input())+1):
-    N, M = map(int, input().split())
-    arr = list(map(int, input().split()))
+    n, m = map(int, input().split())
+    cheese = list(map(int, input().split()))
 
-    Queue = []
-    for i in range(N):
-        Queue.append((i, arr[i]))
-    next_i = N
-    while len(Queue) > 1:
-        idx, ch = Queue.pop(0)
-        if (ch // 2) > 0:
-            Queue.append((idx, (ch // 2)))
-        elif next_i < M:
-            Queue.append((next_i, arr[next_i]))
-            next_i += 1
+    pizza = deque([idx+1, val] for idx, val in enumerate(cheese))
+    last_pizza = 0
+    fire = deque()
+    for _ in range(n):
+        fire.append(pizza.popleft())
 
-    result_idx, result = Queue.pop(0)
-    print(f'#{tc} {result_idx+1}')
+    while fire:
+        idx, chez = fire.popleft()
+        chez //= 2
+        if chez > 0:
+            fire.append([idx, chez])
+        else:
+            if pizza:
+                fire.append(pizza.popleft())
+            else:
+                last_pizza = idx
+    
+    print(f'#{tc} {last_pizza}')
