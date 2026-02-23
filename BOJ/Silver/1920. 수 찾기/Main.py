5fdf5e4e834a6b8ecab44a29bd7ba454748abcb4
Suyoung-Kim <150898767+suweeety@users.noreply.github.com>
2026-02-23 23:17:29 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 52432 KB - DashHub

---
+++ BOJ/Silver/1920. 수 찾기/Main.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+n_list = set(map(int, input().split()))
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+for m in m_list:
+    if m in n_list:
+        print(1)
+    else:
+        print(0)
