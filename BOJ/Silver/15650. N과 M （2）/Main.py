0542054bb4433c18600bd2ce109ef4563ea7a4b5
Suyoung-Kim <150898767+suweeety@users.noreply.github.com>
2026-03-01 16:14:13 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB - DashHub

---
+++ BOJ/Silver/15650. N과 M （2）/Main.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+ans = []
+def dfs():
+    if len(ans) == m:
+        print(*ans)
+    
+    for i in range(1, n + 1):
+        if i not in ans:
+            if len(ans) == 0:
+                ans.append(i)
+                dfs()
+                ans.pop()
+            else:
+                if i > ans[-1]: # 오름차순
+                    ans.append(i)
+                    dfs()
+                    ans.pop()
+dfs()
