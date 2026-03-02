3b6f2e2e95094c30bf6c05c7b35d6f641f17352a
suweety <ktndud2@icloid.com>
2026-03-03 00:30:58 +0900

    미로 탐색 알고리즘 개선: 배열에서 리스트로 변경 및 스택 처리 방식 수정

--- SWEA/5. Stack/10718. ［기본］ 5일차 － 미로 （제출용）/［기본］ 5일차 － 미로 （제출용）.py
+++ SWEA/5. Stack/10718. ［기본］ 5일차 － 미로 （제출용）/［기본］ 5일차 － 미로 （제출용）.py
@@ -1,40 +1,33 @@
 for tc in range(1, int(input())+1):
     N = int(input())
-    arr = [input() for _ in range(N)]
+    maze = [list(map(int, input())) for _ in range(N)]
+    status = False
+    
     # 갈 수 있는 곳 저장
     stack = []
-    result = False
     visited = [[0] * N for _ in range(N)]
-    start = 0
-    end = 0
+
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
     for i in range(N):
         for j in range(N):
-            if arr[i][j] == '2':
-                start = i
-                end = j
-                visited[i][j] = 1
-                stack.append(start)
-                stack.append(end)
-    for i in range(N):
-        for j in range(N):
-            while stack: # 스택이 있으면 돈다
-                end = stack.pop()
-                start = stack.pop()
-                for k in range(4):
-                    r = start + dx[k]
-                    c = end + dy[k]
-                    if 0 <= r < N and 0 <= c < N and visited[r][c] != 1:
-                        temp = arr[r][c]
-                        if temp == '0':
-                            stack.append(r)
-                            stack.append(c)
-                            visited[r][c] = 1
-                        elif temp == '3':
-                            result = True
-                            break
-    if result:
+            if maze[i][j] == '2':
+                si, sj = i, j
+                stack.append((si, sj))
+
+    while stack: # 스택이 있으면 돈다
+        x, y = stack.pop()
+        for k in range(4):
+            r = si + dx[k]
+            c = sj + dy[k]
+            if 0 <= r < N and 0 <= c < N:
+                if maze[r][c] == '0':
+                    stack.append((r, c))
+                    maze[r][c] = 4
+                if maze[r][c] == '3':
+                    status = True
+                    break
+    if status:
         print(f'#{tc} 1')
     else:
         print(f'#{tc} 0')
