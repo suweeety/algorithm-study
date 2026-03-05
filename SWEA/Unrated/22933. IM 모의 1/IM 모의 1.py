f85f0e15018bbb4cb54f65bebe59219af58f91a4
Suyoung-Kim <150898767+suweeety@users.noreply.github.com>
2026-03-05 16:29:50 +0900

    [Unrated] Title: IM 모의 1, Time: 73  ms, Memory: 57216  KB - DashHub

--- SWEA/Unrated/22933. IM 모의 1/IM 모의 1.py
+++ SWEA/Unrated/22933. IM 모의 1/IM 모의 1.py
@@ -8,14 +8,13 @@ for tc in range(1, int(input())+1):
             for k in range(i+1, N):
                 for l in range(j+1, N):
                     if board[i][j] == board[k][l]:
-                        temp = ((i - k) * (j - l)) * 2
+                        temp = (k - i + 1) * (l - j + 1)
                         if cnt < temp:
                             cnt = temp
                             ans = 1
                         elif cnt == temp:
                             ans += 1
-                    # 만약 모든 원소의 숫자가 다르면 싸피부분배열의 면적은 1이고, 개수는 NxN개가 된다.
-
+    # 만약 모든 원소의 숫자가 다르면 싸피부분배열의 면적은 1이고, 개수는 NxN개가 된다.
     if ans == 0:
         ans = N * N
 
